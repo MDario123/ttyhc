cfc578a1df8e14574796b07a349236fd0297e9f8
MDario123 <manuel.dario.oliver@gmail.com>
2025-02-09 19:56:48 +0100

    somewhat style about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "About",
@@ -9,7 +10,15 @@ export default function About() {
   return (
     <div>
       <h1>About</h1>
-      <p>This is the about page</p>
+      <p>You came here to know if this is for you? Yes? Good.</p>
+      <h2>Purpose</h2>
+      <p>Have a good time.</p>
+      <h2>Means</h2>
+      <p>Clicky buttons.</p>
+      <h2>Place</h2>
+      <p>
+        Right <Link href="/">here</Link>.
+      </p>
     </div>
   );
 }
