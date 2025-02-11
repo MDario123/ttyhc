6a3a7df317c733542314965ba1ad6282c09648e4
MDario123 <manuel.dario.oliver@gmail.com>
2025-02-11 14:15:28 +0100

    new clicky and better playground

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 
-import Toggle from "@/app/components/Toggle/Toggle";
+import { clickies, interactiveClickies } from "@/lib/clickies";
+import { JSX, useEffect, useState } from "react";
 
-export default function About() {
-  return (
-    <>
-      <Toggle />
-    </>
-  );
+export default function Playground() {
+  const [clickiesToRender, setClickiesToRender] = useState<JSX.Element[]>([]);
+
+  useEffect(() => {
+    const generatedClickies = [];
+    for (let i = 0; i < 100; i++) {
+      const X =
+        interactiveClickies[
+        clickies[Math.floor(Math.random() * clickies.length)]
+        ];
+      generatedClickies.push(<X key={i} />);
+    }
+    setClickiesToRender(generatedClickies);
+  }, []);
+
+  return <>{clickiesToRender}</>;
 }
--- src/lib/clickies.ts
+++ src/lib/clickies.ts
@@ -1,10 +1,12 @@
+import Button from "@/app/components/Button/Button";
 import Toggle from "@/app/components/Toggle/Toggle";
 import { ReactNode } from "react";
 
-const Clickies = ["toggle"] as const;
+const Clickies = ["toggle", "button"] as const;
 
 export type Clicky = (typeof Clickies)[number];
 export const clickies: Clicky[] = [...Clickies];
 export const interactiveClickies: Record<Clicky, () => ReactNode> = {
   toggle: Toggle,
+  button: Button,
 };
